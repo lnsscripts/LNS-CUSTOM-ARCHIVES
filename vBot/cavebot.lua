3778a843f28117d28291e61d217835537ad32ad5
lnsscripts <gulunas6@gmail.com>
2026-03-20 18:07:16 -0300

    Update cavebot.lua

--- vBot/cavebot.lua
+++ vBot/cavebot.lua
@@ -47,7 +47,6 @@ dofile("/targetbot/creature.lua")
 dofile("/targetbot/creature_attack.lua")
 dofile("/targetbot/creature_editor.lua")
 dofile("/targetbot/creature_priority.lua")
-dofile("/targetbot/looting.lua")
 dofile("/targetbot/walking.lua")
 -- main targetbot file, must be last
 dofile("/targetbot/target.lua")
